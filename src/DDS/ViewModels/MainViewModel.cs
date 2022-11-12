a53174ad9cf494c8abd5b4204593b5783b14fb32
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-12 03:34:43 +0100

    cleanup MainViewModel

--- src/DDS/ViewModels/MainViewModel.cs
+++ src/DDS/ViewModels/MainViewModel.cs
@@ -9,31 +9,13 @@ public sealed partial class MainViewModel : ViewModelBase
     [ObservableProperty] 
     private string _gotPath = "fullPath is empty";
 
-    // public NavigationViewModel Navigation { get; }
-
     // Necessary for Designer:
     public MainViewModel() : this(Globals.Services) { }
 
     [ActivatorUtilitiesConstructor, UsedImplicitly]
-    public MainViewModel(IServiceProvider services
-        // , NavigationViewModel navigation, IAvaloniaEssentials? avaloniaEssentials, 
-        // Lazy<TestViewModel> testViewModel
-        ) : base(services)
+    public MainViewModel(IServiceProvider services) : base(services)
     {
-        // Services = services;
-        _avaloniaEssentials = GetService<IAvaloniaEssentials>()!;
-        // _avaloniaEssentials ??= avaloniaEssentials ?? GetService<IAvaloniaEssentials>()!;
-
-        // HostScreen = Navigation = navigation;
-
-        // GoTest = ReactiveCommand.CreateFromObservable(
-        //     () => Router.Navigate.Execute(GetService<TestViewModel>()),
-        //     canExecute: this.WhenAnyObservable(x => x.Router.CurrentViewModel).Select(x => x is not TestViewModel)
-        // );
-        // GoSecondTest = ReactiveCommand.CreateFromObservable(
-        //     () => Router.Navigate.Execute(GetService<SecondTestViewModel>()), // Transient needs resolvation each cmd execution
-        //     canExecute: this.WhenAnyObservable(x => x.Router.CurrentViewModel).Select(x => x is not SecondTestViewModel)
-        // );
+        _avaloniaEssentials = GetService<IAvaloniaEssentials>();
 
         GoTest = NavigateReactiveCommand<TestViewModel>();
         GoSecondTest = NavigateReactiveCommand<SecondTestViewModel>();

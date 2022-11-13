b35e562c059899249b630a349adcd81e50a403e5
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-13 09:24:18 +0100

    startup cleanup

--- src/DDS/Startup.cs
+++ src/DDS/Startup.cs
@@ -172,17 +172,8 @@ public static class Startup
         services.Add(ServiceDescriptor.Describe(typeof(IViewFor<TViewModel>), 
             p => p.GetRequiredService<TView>(), viewLifetime));
         
-        // if (typeof(TView).IsAssignableTo(typeof(BaseUserControl<TViewModel>)))
-        // {
-        //     services.Add(ServiceDescriptor.Describe(typeof(BaseUserControl<TViewModel>), 
-        //         p => p.GetRequiredService<TView>(), viewLifetime));
-        // }
-        // else if (typeof(TView).IsAssignableTo(typeof(BaseWindow<TViewModel>)))
-        // {
-        //     services.Add(ServiceDescriptor.Describe(typeof(BaseWindow<TViewModel>), 
-        //         p => p.GetRequiredService<TView>(), viewLifetime));
-        // }
         ReactiveViewLocator.DictOfViews[typeof(TViewModel).FullName ?? throw new NullReferenceException()] = typeof(TView);
+        
         return services;
     }
 

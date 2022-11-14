91d957f09a392819479e6e8c59c1a68e2c5ed3ef
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-15 00:16:26 +0100

    Dialog for x-platform Avalonia - native Avalonia Desktop (CoreCLR) impl.

---
+++ src/DDS.Avalonia/Views/DialogWindow.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DDS.Avalonia.Views;
+
+public partial class DialogWindow : BaseWindow<DialogViewModel>
+{
+    // public DialogWindow(WindowBase owner) : this() //=> Owner = owner;
+    // {
+    //     this.WhenActivated(disposable => Owner ??= owner);
+    // }
+
+    [UsedImplicitly]
+    public DialogWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+    
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}

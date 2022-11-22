d2e0d9006772dd11dc299ef9a77a815ab8c75b1b
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-22 20:44:30 +0100

    temp - consider changing IProvdeServices to be Extension Method based

--- src/DDS.Core/Services/IProvideServices.cs
+++ src/DDS.Core/Services/IProvideServices.cs
@@ -8,3 +8,12 @@ public interface IProvideServices
     
     object GetService(Type serviceType);
 }
+
+// public static class ProvideServicesExtensions
+// {
+//     public static TService GetService<TService>(this IProvideServices serviceProviderProvider) where TService : notnull
+//         => serviceProviderProvider.Services.GetRequiredService<TService>();
+//     
+//     public static object GetService(this IProvideServices serviceProviderProvider, Type serviceType)
+//         => serviceProviderProvider.Services.GetRequiredService(serviceType);
+// }

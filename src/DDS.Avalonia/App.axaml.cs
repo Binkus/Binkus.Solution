e92b999cd1f50d98bbda3b0f4feb17ec7ce6ffcb
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-24 02:08:11 +0100

    small cleanup

--- src/DDS.Avalonia/App.axaml.cs
+++ src/DDS.Avalonia/App.axaml.cs
@@ -1,7 +1,9 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using DDS.Core;
+using DDS.Core.Helper;
 
 namespace DDS.Avalonia;
 
@@ -25,36 +27,16 @@ public sealed partial class App : Application, IAppCore
     {
         // Global ServiceProvider available:
         
-        // Task.Run(() => Globals.Services.GetRequiredService<ApplicationViewModel>())
-        //     .ContinueWith(vm => Dispatcher.UIThread.InvokeAsync(
-        //         async () => DataContext = await vm, DispatcherPriority.Background));
         DataContext = Globals.Services.GetRequiredService<ApplicationViewModel>();
         
         var scope = Globals.Services.CreateScope();
         var services = scope.ServiceProvider;
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = services.GetRequiredService<MainWindow>();
             desktop.MainWindow.Height = 960;
             desktop.MainWindow.Width = 690;
-            
-            // Spawn another MainWindow for scope testing
-
-            // Task.Run(async () =>
-            // {
-            //     await 4.Seconds();
-            //     
-            //     Dispatcher.UIThread.Post(() =>
-            //     {
-            //         sw = Stopwatch.StartNew();
-            //         var window2 = Globals.Services.CreateAsyncScope().ServiceProvider.GetRequiredService<MainWindow>();
-            //         Console.WriteLine(sw.ElapsedMilliseconds);
-            //         window2.Show();
-            //
-            //         window2.Height = 960;
-            //         window2.Width = 690;
-            //     }, DispatcherPriority.Background);
-            // });
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {

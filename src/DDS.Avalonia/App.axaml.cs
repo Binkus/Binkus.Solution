06043a12829e1b9ba741d79c30dcd67f55ac05a9
Binkus <11396231+Binkus@users.noreply.github.com>
2022-11-24 02:05:51 +0100

    create and use new ServiceScope on App instance creation (not required)
    but MS DI feels better when resolving scoped services from a new ServiceScope, that allows using Scope Validation (which would complain when using the root scope for resolving scoped services). And added example for spawning second MainWindow, scopes work there as expected, both when using root scope or when creating scope at beginning: Both windows very independent from each other, only Singleton (e.g. ViewModels) that get resolved are shared between the sub services of the MainViewModels and MainWindowViewModels - DataBinding working as expected, when e.g. Singleton ViewModel resolved, a change on Data on one Window updates the second one immediately perfectly. So we using scopes as effectively Singleton - but only Single per instance (of e.g. MainWindowViewModel), so all sub ViewModels have same scope and resolve all the same shared Services, but when creating new scope and another Window, it gets it's own scope and their own scoped Services; so only real Singletons are shared between everything; basically as intended.
    A ScopeManagerService (Singleton) could be handy, especially when trying to get a specific scope from e.g. the Android platform specific project. Those scopes could be used to soft restart the App on SingleViewApplicationLifetime, and on Desktops as well by just respawning the MainWindow with another scope. The ScopeManagerService could allow additional cross-scope communication.
    We need to get the currently active or viewed Scope for SingleViewApplicationLifetime, to properly bind the Avalonia TopLevel (from TopLevelService) and get the correct one, cause currently e.g. the Android project only knows and needs the Global Scope.
    
    (When spawning MainWindow again, some controls seem partially broken for some reason, regarding the work-in-progress HamburgerMenu; broken in the sense that they behave and look kind of differently, even though it works still; the bug(s) are very likely in HamburgerMenu or in App.axaml.)

--- src/DDS.Avalonia/App.axaml.cs
+++ src/DDS.Avalonia/App.axaml.cs
@@ -24,18 +24,41 @@ public sealed partial class App : Application, IAppCore
     public override void OnFrameworkInitializationCompleted()
     {
         // Global ServiceProvider available:
+        
+        // Task.Run(() => Globals.Services.GetRequiredService<ApplicationViewModel>())
+        //     .ContinueWith(vm => Dispatcher.UIThread.InvokeAsync(
+        //         async () => DataContext = await vm, DispatcherPriority.Background));
         DataContext = Globals.Services.GetRequiredService<ApplicationViewModel>();
-        // var scope = Globals.Services.CreateScope();
-        // var services = scope.ServiceProvider;
-        var services = Globals.Services; // we use default scope for first instances of Main*
+        
+        var scope = Globals.Services.CreateScope();
+        var services = scope.ServiceProvider;
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = services.GetRequiredService<MainWindow>();
             desktop.MainWindow.Height = 960;
             desktop.MainWindow.Width = 690;
+            
+            // Spawn another MainWindow for scope testing
+
+            // Task.Run(async () =>
+            // {
+            //     await 4.Seconds();
+            //     
+            //     Dispatcher.UIThread.Post(() =>
+            //     {
+            //         sw = Stopwatch.StartNew();
+            //         var window2 = Globals.Services.CreateAsyncScope().ServiceProvider.GetRequiredService<MainWindow>();
+            //         Console.WriteLine(sw.ElapsedMilliseconds);
+            //         window2.Show();
+            //
+            //         window2.Height = 960;
+            //         window2.Width = 690;
+            //     }, DispatcherPriority.Background);
+            // });
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
+            services = Globals.Services; // as long as no ScopeManagerService exists
             singleViewPlatform.MainView = services.GetRequiredService<MainView>();
         }
 

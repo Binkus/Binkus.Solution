8ed2cd17ae943c4f5acb1755297d57bcdcf545b0
Binkus <11396231+Binkus@users.noreply.github.com>
2022-12-04 20:57:11 +0100

    temp - rm awaitable Process example

--- src/DDS.Avalonia.Desktop/Program.cs
+++ src/DDS.Avalonia.Desktop/Program.cs
@@ -23,59 +23,11 @@ namespace DDS.Avalonia.Desktop
         // Avalonia configuration, don't remove; also used by visual designer.
         private static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
-                .DoSomeTests().GetAwaiter().GetResult()
                 .ConfigureAppServices(services => services
                         .AddSingleton<ICloseAppService,CloseAppService>()
                         .AddScoped<IDialogAlertMessageBox,DialogAlertMessageBox>()
                     )
                 .UsePlatformDetect()
                 .LogToTrace();
-
-        private static async ValueTask<AppBuilder> DoSomeTests(this AppBuilder appBuilder)
-        {
-            // var t1 = Task.Run(async() =>
-            // {
-            //     await Task.Delay(1000);
-            //     Console.WriteLine("1s");
-            //     return "";
-            // });
-            // var t2 = Task.Run(async () =>
-            // {
-            //     await Task.Delay(2000);
-            //     Console.WriteLine("2s Exception");
-            //     throw new EndOfStreamException();
-            //     return "";
-            // });
-            // var t3 = Task.Run(async() =>
-            // {
-            //     await Task.Delay(9000);
-            //     Console.WriteLine("9s");
-            //     return "";
-            // });
-            //
-            // await TaskExtensions.WhenAll(t1, t2, t3);
-            
-            //
-
-            // var process = new Process();
-            // // Configure the process using the StartInfo properties.
-            // process.StartInfo.FileName = "konsole";
-            // // process.StartInfo.Arguments = "-n";
-            // process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-            // process.Start();
-            //
-            // // var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(0));
-            // // var processTask = process.ToCancelableTask(tokenSource.Token);
-            //
-            // var r = await Task.Run(async () =>
-            // {
-            //     // await Task.Delay(10000);
-            //     return await process;
-            // });
-            //
-            //
-            //
-            return appBuilder;
-        }
     }
 }

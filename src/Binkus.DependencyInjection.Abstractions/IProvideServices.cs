028285e8b7d4169e54cd6da00df7eb5235e56347
Binkus <11396231+Binkus@users.noreply.github.com>
2022-12-29 09:30:05 +0100

    fix IProvideServices docs (cause global using MEDI removed, using MEDI not working for inherit docs)

--- src/Binkus.DependencyInjection.Abstractions/IProvideServices.cs
+++ src/Binkus.DependencyInjection.Abstractions/IProvideServices.cs
@@ -19,33 +19,33 @@ public static class ProvideServicesExtensions
     // not required
     //
     
-    /// <summary><inheritdoc cref="ServiceProviderServiceExtensions.GetService{T}"/></summary>
+    /// <summary><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetService{T}"/></summary>
     /// <param name="serviceProviderProvider">The <see cref="IProvideServices"/> to retrieve the
     /// <see cref="IServiceProvider"/> object from to retrieve the service object from.</param>
-    /// <returns><inheritdoc cref="ServiceProviderServiceExtensions.GetService{T}"/></returns>
+    /// <returns><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetService{T}"/></returns>
     /// <exception cref="System.InvalidOperationException">
-    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetService{T}"/></exception>
+    /// <inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetService{T}"/></exception>
     public static TService? GetService<TService>(this IProvideServices serviceProviderProvider)
         => serviceProviderProvider.Services.GetService<TService>();
     
     //
     
-    /// <summary><inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService"/></summary>
+    /// <summary><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService"/></summary>
     /// <param name="serviceProviderProvider">The <see cref="IProvideServices"/> to retrieve the
     /// <see cref="IServiceProvider"/> object from to retrieve the service object from.</param>
-    /// <param name="serviceType"><inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService"/></param>
-    /// <returns><inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService"/></returns>
+    /// <param name="serviceType"><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService"/></param>
+    /// <returns><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService"/></returns>
     /// <exception cref="System.InvalidOperationException">
-    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService"/></exception>
+    /// <inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService"/></exception>
     public static object GetRequiredService(this IProvideServices serviceProviderProvider, Type serviceType)
         => serviceProviderProvider.Services.GetRequiredService(serviceType);
 
-    /// <summary><inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService{T}"/></summary>
+    /// <summary><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService{T}"/></summary>
     /// <param name="serviceProviderProvider">The <see cref="IProvideServices"/> to retrieve the
     /// <see cref="IServiceProvider"/> object from to retrieve the service object from.</param>
-    /// <returns><inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService{T}"/></returns>
+    /// <returns><inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService{T}"/></returns>
     /// <exception cref="System.InvalidOperationException">
-    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService{T}"/></exception>
+    /// <inheritdoc cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService{T}"/></exception>
     public static TService GetRequiredService<TService>(this IProvideServices serviceProviderProvider)
         where TService : notnull => serviceProviderProvider.Services.GetRequiredService<TService>();
 }

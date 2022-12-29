4aeabfb05c1b85387dbcdaa4e6f5291006437ced
Binkus <11396231+Binkus@users.noreply.github.com>
2022-12-29 07:47:23 +0100

    refac IProvideServices

--- src/Binkus.ReactiveMvvm/IProvideServices.cs
+++ src/Binkus.ReactiveMvvm/IProvideServices.cs
@@ -4,16 +4,16 @@ public interface IProvideServices
 {
     IServiceProvider Services { get; }
 
-    TService GetService<TService>() where TService : notnull;
-    
-    object GetService(Type serviceType);
+    // TService GetService<TService>() where TService : notnull;
+    //
+    // object GetService(Type serviceType);
 }
 
-// public static class ProvideServicesExtensions
-// {
-//     public static TService GetService<TService>(this IProvideServices serviceProviderProvider) where TService : notnull
-//         => serviceProviderProvider.Services.GetRequiredService<TService>();
-//     
-//     public static object GetService(this IProvideServices serviceProviderProvider, Type serviceType)
-//         => serviceProviderProvider.Services.GetRequiredService(serviceType);
-// }
+public static class ProvideServicesExtensions
+{
+    public static TService GetService<TService>(this IProvideServices serviceProviderProvider) where TService : notnull
+        => serviceProviderProvider.Services.GetRequiredService<TService>();
+    
+    public static object GetService(this IProvideServices serviceProviderProvider, Type serviceType)
+        => serviceProviderProvider.Services.GetRequiredService(serviceType);
+}

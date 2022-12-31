f6e9e21be8d0198100346e4d7c5e163af668e546
Binkus <11396231+Binkus@users.noreply.github.com>
2022-12-31 09:58:26 +0100

    refac StopwatchExtensions (no behavior change)

--- src/Binkus.Extensions.Stopwatch/StopwatchExtensions.cs
+++ src/Binkus.Extensions.Stopwatch/StopwatchExtensions.cs
@@ -14,12 +14,14 @@ public static class StopwatchExtensions
     private static readonly double s_tickFrequency = 10_000_000.0 / Stopwatch.Frequency; // e.g. ~== 0.01 (depending on hardware&OS)
     private static long GetTimestampCompatibleTicks(long ticks) => (long)(ticks / s_tickFrequency); // e.g. ~== ticks * 100L
     
-#if NET7_0_OR_GREATER
-
+    //
+    
     public static long AddTimestamp(this int ticks) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(ticks);
     public static long AddTimestamp(this long ticks) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(ticks);
     public static long AddTimestamp(this TimeSpan timeSpan) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(timeSpan.Ticks);
     
+#if NET7_0_OR_GREATER
+
     public static TimeSpan GetElapsedTime(this long startingTimestamp) => Stopwatch.GetElapsedTime(startingTimestamp);
     public static TimeSpan GetElapsedTime(this long startingTimestamp, long endingTimestamp)
         => Stopwatch.GetElapsedTime(startingTimestamp, endingTimestamp);
@@ -44,7 +46,7 @@ public static class StopwatchExtensions
     public static double GetElapsedMinutes(this long startingTimestamp, long endingTimestamp)
         => startingTimestamp.GetElapsedSeconds(endingTimestamp) / 60d;
 
-#else // e.g. NETSTANDARD2_0 || NETSTANDARD2_1
+#else // e.g. NETSTANDARD2_0 || NETSTANDARD2_1 || NET6_0 || NET5_0
     
     private static TimeSpan StopwatchGetElapsedTime(this long startingTimestamp, long endingTimestamp) =>
         new((long)((endingTimestamp - startingTimestamp) * s_tickFrequency));
@@ -53,10 +55,6 @@ public static class StopwatchExtensions
         StopwatchGetElapsedTime(startingTimestamp, Stopwatch.GetTimestamp());
     
     //
-
-    public static long AddTimestamp(this int ticks) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(ticks);
-    public static long AddTimestamp(this long ticks) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(ticks);
-    public static long AddTimestamp(this TimeSpan timeSpan) => Stopwatch.GetTimestamp() - GetTimestampCompatibleTicks(timeSpan.Ticks);
     
     public static TimeSpan GetElapsedTime(this long startingTimestamp) => StopwatchGetElapsedTime(startingTimestamp);
     public static TimeSpan GetElapsedTime(this long startingTimestamp, long endingTimestamp)

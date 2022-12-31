3b7c5864b6d8f3082a2d6ba49c0cc621224d935c
Binkus <11396231+Binkus@users.noreply.github.com>
2022-12-31 02:15:10 +0100

    rm LazyExtensions

--- src/DDS.Core/Helper/LazyExtensions.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.CompilerServices;
-
-namespace DDS.Core.Helper;
-
-public static class LazyExtensions
-{
-    public static ValueTaskAwaiter<T> GetAwaiter<T>(this Lazy<T> lazy) => ValueTask.FromResult(lazy.Value).GetAwaiter();
-}

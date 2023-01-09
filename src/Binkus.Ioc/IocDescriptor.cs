43619563c93b5d4a84e26c662c3ee8e4a3acc9ba
Binkus <11396231+Binkus@users.noreply.github.com>
2023-01-09 21:21:07 +0100

    update & refac IocDescriptor, almost finalized (mainly docs missing)

--- src/Binkus.Ioc/IocDescriptor.cs
+++ src/Binkus.Ioc/IocDescriptor.cs
@@ -4,8 +4,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Binkus.DependencyInjection;
 
-public delegate object ServiceFactory(IServiceProvider services);
-
 public sealed class IocDescriptor : IEquatable<IocDescriptor>
 {
     [SetsRequiredMembers]
@@ -16,85 +14,103 @@ public sealed class IocDescriptor : IEquatable<IocDescriptor>
     {
         ArgumentNullException.ThrowIfNull(serviceType);
         ArgumentNullException.ThrowIfNull(implType);
-        Lifetime = lifetime;
+        _lifetime = ThrowOnInvalidLifetime(lifetime);
         ServiceType = serviceType;
         ImplType = implType;
+        
+        ThrowIfImplTypeIsNotAssignableToServiceType();
     }
     
     [SetsRequiredMembers]
-    public IocDescriptor(IocLifetime lifetime, Type serviceType, ServiceFactory factory)
+    public IocDescriptor(IocLifetime lifetime, Type serviceType, Func<IServiceProvider, object> factory)
     {
         ArgumentNullException.ThrowIfNull(serviceType);
         ArgumentNullException.ThrowIfNull(factory);
-        Lifetime = lifetime;
+        _lifetime = ThrowOnInvalidLifetime(lifetime);
         ServiceType = serviceType;
-        ImplFactory = factory;
+        Factory = factory;
     }
     
     [SetsRequiredMembers]
-    public IocDescriptor(Type serviceType, object implementation)
+    public IocDescriptor(Type serviceType, object implementation, bool scoped = false)
     {
         ArgumentNullException.ThrowIfNull(serviceType);
         ArgumentNullException.ThrowIfNull(implementation);
-        Lifetime = IocLifetime.Singleton;
+        _lifetime = scoped ? IocLifetime.Scoped : IocLifetime.Singleton;
         ServiceType = serviceType;
-        // ImplType = implementation.GetType();
+        ImplType = implementation.GetType();
         Implementation = implementation;
+        
+        ThrowIfImplTypeIsNotAssignableToServiceType();
     }
     
     [SetsRequiredMembers]
-    public IocDescriptor(IocDescriptor descriptor)
+    internal IocDescriptor(IocDescriptor descriptor)
     {
+        ArgumentNullException.ThrowIfNull(descriptor);
+        ArgumentNullException.ThrowIfNull(descriptor.ServiceType);
+        _lifetime = ThrowOnInvalidLifetime(descriptor.Lifetime);
         ServiceType = descriptor.ServiceType;
         ImplType = descriptor.ImplType;
-        Lifetime = descriptor.Lifetime;
-        ImplFactory = descriptor.ImplFactory;
-        // Factory = descriptor.Factory; // don't cause descriptor.Factory captures property Implementation of param. descriptor 
+        Factory = descriptor.Factory;
+        Implementation = descriptor.Implementation;
+        
+        if (ImplType is null && Factory is null && Implementation is null)
+            throw new InvalidOperationException($"Invalid {nameof(IocDescriptor)}");
+
+        ThrowIfImplTypeIsNotAssignableToServiceType();
     }
+    
+    internal IocDescriptor Copy() => new(this);
 
     internal IocDescriptor() { }
 
-    internal IocDescriptor CreateForScope() => new(this);
+    internal void ThrowIfImplTypeIsNotAssignableToServiceType()
+    {
+        if (!ImplType?.IsAssignableTo(ServiceType) ?? false)
+            throw new InvalidOperationException(
+                $"{Implementation}'s Type {ImplType} can't be assigned to {ServiceType}");
+    }
+    
+    internal void ThrowOnInvalidity()
+    {
+        ThrowOnInvalidLifetime(Lifetime);
+        ArgumentNullException.ThrowIfNull(ServiceType);
+        if (ImplType is null && Factory is null && Implementation is null)
+            throw new InvalidOperationException($"Invalid {nameof(IocDescriptor)}");
+        
+        ThrowIfImplTypeIsNotAssignableToServiceType();
+    }
+    
+    private static IocLifetime ThrowOnInvalidLifetime(IocLifetime lifetime)
+    {
+        if (lifetime is IocLifetime.Scoped or IocLifetime.Singleton or IocLifetime.Transient)
+            return lifetime;
+        throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, $"Invalid {nameof(IocLifetime)}");
+    }
 
-    public required IocLifetime Lifetime { get; init; }
+    private readonly IocLifetime _lifetime;
+    public required IocLifetime Lifetime
+    {
+        get => _lifetime;
+        init => _lifetime = ThrowOnInvalidLifetime(value);
+    }
+
     public required Type ServiceType { get; init; }
     
     [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
-    public Type? ImplType { get; init; } // potentially: ?? Implementation?.GetType();
-    internal object? Implementation { get; set; }
-
-    private readonly ServiceFactory? _implFactory;
-    public ServiceFactory? ImplFactory
-    {
-        get => _implFactory;
-        init
-        {
-            Factory = _implFactory = value;
-            if (value is null || Lifetime is IocLifetime.Transient) return;
-            Factory = services =>
-            {
-                // if (Implementation is not null) return Implementation;
-                lock (this) return Implementation ??= value.Invoke(services);
-                
-                // ImplType = implementation.GetType();
-            };
-        }
-    }
-
-    internal ServiceFactory? Factory { get; init; }
+    public Type? ImplType { get; init; }
+    internal object? Implementation { get; init; }
+    internal Func<IServiceProvider, object>? Factory { get; init; }
     
-    internal IocDescriptor? Next { get; set; }
-    
-    // Equality: (x is equal to y when every field (except ignored Implementation) is equal)
-    // Implementation is ignored, because the Descriptor of root container shall be equivalent
-    // to the one of a scoped container, independent from if Implementation has been cached or created differently or not 
+    // Equality
 
     public bool Equals(IocDescriptor? other)
     {
         if (ReferenceEquals(null, other)) return false;
         if (ReferenceEquals(this, other)) return true;
         return Lifetime == other.Lifetime && ServiceType == other.ServiceType && ImplType == other.ImplType &&
-               Equals(ImplFactory, other.ImplFactory);
+               Equals(Factory, other.Factory) && Equals(Implementation, other.Implementation);
     }
 
     public override bool Equals(object? obj)
@@ -104,7 +120,7 @@ public sealed class IocDescriptor : IEquatable<IocDescriptor>
 
     public override int GetHashCode()
     {
-        return HashCode.Combine((int)Lifetime, ServiceType, ImplType, ImplFactory);
+        return HashCode.Combine((int)Lifetime, ServiceType, ImplType, Factory, Implementation);
     }
 
     public static bool operator ==(IocDescriptor? left, IocDescriptor? right)
@@ -116,4 +132,56 @@ public sealed class IocDescriptor : IEquatable<IocDescriptor>
     {
         return !Equals(left, right);
     }
+    
+    //
+    // Static creation helper:
+
+    public static IocDescriptor CreateSingleton(Type serviceType, object implementation) => new(serviceType, implementation);
+    public static IocDescriptor CreateScoped(Type serviceType, object implementation) => new(serviceType, implementation, true);
+    
+    public static IocDescriptor CreateSingleton<T>(T implementation) where T : notnull => new(typeof(T), implementation);
+    public static IocDescriptor CreateScoped<T>(T implementation) where T : notnull => new(typeof(T), implementation, true);
+    
+    //
+    
+    public static IocDescriptor CreateSingleton(Type serviceType, Func<IServiceProvider, object> factory) => new(IocLifetime.Singleton, serviceType, factory);
+    public static IocDescriptor CreateScoped(Type serviceType, Func<IServiceProvider, object> factory) => new(IocLifetime.Scoped, serviceType, factory);
+    public static IocDescriptor CreateTransient(Type serviceType, Func<IServiceProvider, object> factory) => new(IocLifetime.Transient, serviceType, factory);
+    
+    public static IocDescriptor CreateSingleton<T>(Func<IServiceProvider, T> factory) where T : class => new(IocLifetime.Singleton, typeof(T), factory) { ImplType = typeof(T) }; // ImplType not really needed
+    public static IocDescriptor CreateScoped<T>(Func<IServiceProvider, T> factory) where T : class => new(IocLifetime.Scoped, typeof(T), factory) { ImplType = typeof(T) }; // ImplType not really needed
+    public static IocDescriptor CreateTransient<T>(Func<IServiceProvider, T> factory) where T : class => new(IocLifetime.Transient, typeof(T), factory) { ImplType = typeof(T) }; // ImplType not really needed
+    
+    //
+    
+    public static IocDescriptor CreateSingleton(Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implType) => new(IocLifetime.Singleton, serviceType, implType);
+    public static IocDescriptor CreateScoped(Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implType) => new(IocLifetime.Scoped, serviceType, implType);
+    public static IocDescriptor CreateTransient(Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implType) => new(IocLifetime.Transient, serviceType, implType);
+    
+    public static IocDescriptor CreateSingleton<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImpl>() where TImpl : TService => new(IocLifetime.Singleton, typeof(TService), typeof(TImpl));
+    public static IocDescriptor CreateScoped<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImpl>() where TImpl : TService=> new(IocLifetime.Scoped, typeof(TService), typeof(TImpl));
+    public static IocDescriptor CreateTransient<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImpl>() where TImpl : TService => new(IocLifetime.Transient, typeof(TService), typeof(TImpl));
+    
+    //
+    // Runtime lifetime:
+    
+    public static IocDescriptor Create(Type serviceType, object implementation, bool scoped = false) => new(serviceType, implementation, scoped);
+    public static IocDescriptor Create(IocLifetime lifetime, Type serviceType, object implementation) => new(serviceType, implementation, lifetime is not IocLifetime.Transient ? lifetime is IocLifetime.Scoped : throw new InvalidOperationException());
+    
+    //
+    
+    public static IocDescriptor Create<T>(IocLifetime lifetime, T implementation) where T : notnull => new(typeof(T), implementation, lifetime is not IocLifetime.Transient ? lifetime is IocLifetime.Scoped : throw new InvalidOperationException());
+    public static IocDescriptor Create<T>(T implementation, bool scoped = false) where T : notnull => new(typeof(T), implementation, scoped);
+    
+    //
+    
+    public static IocDescriptor Create(IocLifetime lifetime, Type serviceType, Func<IServiceProvider, object> factory) => new(lifetime, serviceType, factory);
+    
+    public static IocDescriptor Create<T>(IocLifetime lifetime, Func<IServiceProvider, T> factory) where T : class => new(lifetime, typeof(T), factory) { ImplType = typeof(T) }; // ImplType not really needed
+    
+    //
+    
+    public static IocDescriptor Create(IocLifetime lifetime, Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implType) => new(lifetime, serviceType, implType);
+    
+    public static IocDescriptor Create<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImpl>(IocLifetime lifetime) where TImpl : TService => new(lifetime, typeof(TService), typeof(TImpl));
 }
